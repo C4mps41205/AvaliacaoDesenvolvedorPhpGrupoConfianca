9e42f9e5f925640803e203406f7c747f1cbc0598
C4mps41205 <lucasmmpacheco75@gmail.com>
2024-01-20 00:13:16 -0300

    feat: initializing database settings

---
+++ app/model/database_credentials.php
@@ -0,0 +1,106 @@
+<?php
+
+class DatabaseCredentials
+{
+    public string $host;
+    public string $databaseName;
+    public string $username;
+    public string $password;
+
+    /**
+     * Get the value of host
+     *
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * Set the value of host
+     *
+     * @param string $host
+     *
+     * @return self
+     */
+    public function setHost(string $host): self
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of databaseName
+     *
+     * @return string
+     */
+    public function getDatabaseName(): string
+    {
+        return $this->databaseName;
+    }
+
+    /**
+     * Set the value of databaseName
+     *
+     * @param string $databaseName
+     *
+     * @return self
+     */
+    public function setDatabaseName(string $databaseName): self
+    {
+        $this->databaseName = $databaseName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     *
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @param string $username
+     *
+     * @return self
+     */
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     *
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @param string $password
+     *
+     * @return self
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
+?>
---
+++ app/settings/database.php
@@ -0,0 +1,50 @@
+<?php
+include '../model/database_credentials.php';
+
+class Database
+{
+    private $conn;
+    private $model;
+
+    public function __construct(DatabaseCredentials $databaseCredentials)
+    {
+        $this->model = $this->SetCredentials($databaseCredentials);
+    }
+
+    public function SetCredentials(DatabaseCredentials $model): DatabaseCredentials
+    {
+        $model  ->setHost($_SERVER['REMOTE_ADDR'])
+                ->setDatabaseName('grupo-confianca-db')
+                ->setUsername("root")
+                ->setPassword("root-grupo-confianca");
+        
+        return $model;
+    }
+
+    public function CreateDb() : PDO
+    { 
+        try 
+        {
+            $host = $this->model->getHost();
+            $databaseName = $this->model->getDatabaseName();
+            $username = $this->model->getUsername();
+            $password = $this->model->getPassword();
+
+            $this->conn = new PDO("mysql:host=".$host.";dbname=".$databaseName, $username, $password);
+            
+            if(!$this->conn)
+            {
+                throw new Exception("Algo deu errado na hora de se conectar ao banco de dados.");
+                die();
+            }
+
+            return $this->conn;
+        } catch (Exception $exception) 
+        {
+            echo $exception->getMessage();
+        }
+    }
+}
+
+$teste = new Database(new DatabaseCredentials());   
+?>
